3e964da5a19aef375747aec0af47537fba99cf2f
Taiki <tyoshikawa1106@gmail.com>
2015-08-26 22:37:43 +0900

    commit

---
+++ app/controllers/components_controller.rb
@@ -0,0 +1,97 @@
+class ComponentsController < ApplicationController
+  def index
+  end
+
+  def activity_timeline
+  end
+
+  def badges
+  end
+
+  def breadcrumbs
+  end
+
+  def buttons
+  end
+
+  def button_groups
+  end
+
+  def cards
+  end
+
+  def data_tables
+  end
+
+  def datepickers
+  end
+
+  def forms
+  end
+
+  def grid_system
+  end
+
+  def images
+  end
+
+  def lookups
+  end
+
+  def advanced_modal
+  end
+
+  def media_objects
+  end
+
+  def notifications
+  end
+
+  def page_headers
+  end
+
+  def picklists
+  end
+
+  def pills 
+  end
+
+  def popovers
+  end
+
+  def spinners
+  end
+
+  def tabs
+  end
+
+  def tiles
+  end
+
+  def tooltips
+  end
+
+  def trees
+  end
+
+  def floats
+  end
+
+  def lists
+  end
+
+  def scrollable
+  end
+
+  def text
+  end
+
+  def themes
+  end
+
+  def truncation
+  end
+
+  def visibility
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LightningDesignApp::Application.config.session_store :cookie_store, key: '_lightning_design_app_session'
